288c109a9dc3d7d27a5c3186984ca84e774abfa6
Bizzaroerik <epugesek@gmail.com>
2025-03-12 14:16:16 -0500

    add dialogue manager

---
+++ Scripts/Characters/NPC/guide.gd
@@ -0,0 +1,38 @@
+extends Node
+
+var balloon_scene = preload("res://Dialogue/game_dialogue_balloon.tscn")
+
+@onready var interactable: Interactable = $Interactable
+@onready var interactable_label: Control = $InteractableLabel
+
+var in_range: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	interactable.interactable_activated.connect(on_interactable_activated)
+	interactable.interactable_deactivated.connect(on_interactable_deactivated)
+	interactable_label.hide()
+	
+	GameDialogueManager.give_crop_seeds.connect(on_give_crop_seeds)
+
+func on_interactable_activated() -> void:
+	interactable_label.show()
+	in_range = true
+func on_interactable_deactivated() -> void:
+	interactable_label.hide()
+	in_range = false
+
+func _process(delta: float) -> void:
+	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("show_dialogue") and in_range:
+		var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
+		get_tree().current_scene.add_child(balloon)
+		balloon.start(load("res://Dialogue/Convos/guide.dialogue"), "start")
+	
+func on_give_crop_seeds() -> void:
+	ToolManager.enable_tool_button(DataTypes.Tools.ChopWood)
+	ToolManager.enable_tool_button(DataTypes.Tools.TillGround)
+	ToolManager.enable_tool_button(DataTypes.Tools.WaterCrops)
+	ToolManager.enable_tool_button(DataTypes.Tools.PlantCrops)
--- Scripts/UI/PlayerOverlay/tools_panel.gd
+++ Scripts/UI/PlayerOverlay/tools_panel.gd
@@ -5,6 +5,17 @@ extends PanelContainer
 @onready var tool_watering: Button = $MarginContainer/HBoxContainer/ToolWatering
 @onready var tool_corn: Button = $MarginContainer/HBoxContainer/ToolCorn
 
+func _ready() -> void:
+	
+	ToolManager.enable_tool.connect(on_enable_tool_button)
+	tool_axe.disabled = true
+	tool_axe.focus_mode = Control.FOCUS_NONE
+	tool_tilling.disabled = true
+	tool_tilling.focus_mode = Control.FOCUS_NONE
+	tool_watering.disabled = true
+	tool_watering.focus_mode = Control.FOCUS_NONE
+	tool_corn.disabled = true
+	tool_corn.focus_mode = Control.FOCUS_NONE
 
 func _on_tool_axe_pressed() -> void:
 	ToolManager.set_tool(DataTypes.Tools.ChopWood)
@@ -30,3 +41,17 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _on_tool_tomato_pressed() -> void:
 	pass # Replace with function body.
+
+func on_enable_tool_button(tool: DataTypes.Tools) -> void:
+	if tool == DataTypes.Tools.ChopWood:
+		tool_axe.disabled = false
+		tool_axe.focus_mode = Control.FOCUS_ALL
+	elif tool == DataTypes.Tools.TillGround:
+		tool_tilling.disabled = false
+		tool_tilling.focus_mode = Control.FOCUS_ALL
+	elif tool == DataTypes.Tools.WaterCrops:
+		tool_watering.disabled = false
+		tool_watering.focus_mode = Control.FOCUS_ALL
+	elif tool == DataTypes.Tools.PlantCrops:
+		tool_corn.disabled = false
+		tool_corn.focus_mode = Control.FOCUS_ALL
