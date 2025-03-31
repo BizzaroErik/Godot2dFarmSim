48a1b257c26efad8c4e19db31ec0ce3291567378
Bizzaroerik <epugesek@gmail.com>
2025-03-31 13:12:40 -0500

    add tree hit sfx

--- Scripts/Objects/small_tree.gd
+++ Scripts/Objects/small_tree.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 
 @onready var hurtable: Hurtable = $Hurtable
 @onready var health: Health = $Health
+@onready var tree_hit_sfx: AudioStreamPlayer2D = $TreeHitSfx
 
 var log_scene = preload("res://Scenes/Objects/Collectables/Log.tscn")
 
@@ -10,6 +11,7 @@ func _ready() -> void:
 	health.dead.connect(on_max_damage_reached)
 
 func on_hurt(hit_damage: int) -> void:
+	tree_hit_sfx.play()
 	health.apply_damage(hit_damage)
 	material.set_shader_parameter("shake_intensity", 0.5)
 	await get_tree().create_timer(1.0).timeout
@@ -21,5 +23,6 @@ func on_max_damage_reached() -> void:
 
 func add_log() -> void:
 	var object_log = log_scene.instantiate() as Node2D
+	print(global_position)
 	object_log.global_position = global_position
 	get_parent().add_child(object_log)
